6e54be8b76a9bda06087f3748f393dfa784d737b
LeJohne <johngoy11024@gmail.com>
2019-12-11 21:13:38 -0500

    commit 15 - finishing touches

--- js/shapes.js
+++ js/shapes.js
@@ -6,7 +6,7 @@ window.onload = function() {
     document.getElementById("smile-sample").onclick = drawFaceStaff;
     document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
 
-    // Eleemnt ID Below Here
+    // Eleemnt ID Below Here.
 
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
